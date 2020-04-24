49c7409957b201f0b20b83f60de62a21c2dca936
jos3duardo <jos3duardolopes@gmail.com>
2020-04-24 13:08:34 -0400

    create method login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,10 +6,22 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use AuthenticatesUsers;
+
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        $credentials = $this->credentials($request);
+
+        $token = \JWTAuth::attempt($credentials);
+    }
+
+
 
 
 }
